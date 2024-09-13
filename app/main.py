727798ee576e66a530f8cb2838cdd5a171896d42
ARTEM <artemsmirnof@mail.ru>
2024-09-13 04:28:34 +0300

    dlya docker compose

--- app/main.py
+++ app/main.py
@@ -37,5 +37,3 @@ async def translate(request: TranslationRequest):
         raise HTTPException(status_code=500, detail=f"Ошибка перевода: {str(e)}")
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
